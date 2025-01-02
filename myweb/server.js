f7be7bc849c6cc8146ac5292b7549eb3933e78ee
Kagami5464 <145747586+Kagami5464@users.noreply.github.com>
2025-01-03 00:21:08 +0800

    Delete myweb directory

--- myweb/server.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var server = express();
-
-server.use(express.static(__dirname + "/web"));
-
-server.listen(80, () => {
-    console.log("Server is running at port 80.");
-});
