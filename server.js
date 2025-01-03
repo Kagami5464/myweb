ce749e3d7ae647e9e712a3ecce1150e828c20d4c
Kagami5464 <a0976919813@gmail.com>
2025-01-03 11:52:46 +0800

    1140103

--- server.js
+++ server.js
@@ -6,3 +6,18 @@ server.use(express.static(__dirname + "/web"));
 server.listen(80, () => {
     console.log("Server is running at port 80.");
 });
+
+var DB = require("nedb-promises");
+var ProfolioDB = DB.create(__dirname+"/protfolio.db");
+
+ProfolioDB.insert([
+    { href："#portfolioModal1",}
+])
+
+server.get("/services",(req,res)=>{
+    //DB find
+    var Services=[
+        {icon:"fa-shopping-cart",heading:"E-Commerce",text:"L"}
+        {icon:"fa-laptop",heading:"Design",text:"L"}
+    ]
+}
