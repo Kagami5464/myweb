68ef63483cc091c8ef1c08d3e4bd7cb278382a9f
Kagami5464 <a0976919813@gmail.com>
2025-01-08 11:51:46 +0800

    1140108

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ server.listen(80, () => {
 var DB = require("nedb-promises");
 var ProfolioDB = DB.create(__dirname+"/protfolio.db");
 
-ProfolioDB.insert([
-    { href："#portfolioModal1",}
-])
+//ProfolioDB.insert([
+//    { href:"#portfolioModal1"}
+//])
 
 server.get("/services",(req,res)=>{
     //DB find
     var Services=[
-        {icon:"fa-shopping-cart",heading:"E-Commerce",text:"L"}
+        {icon:"fa-shopping-cart",heading:"E-Commerce",text:"L"},
         {icon:"fa-laptop",heading:"Design",text:"L"}
     ]
 })
