ca2164bdacf5f25c3a0eefa929c1ebc58461d59b
Kagami5464 <a0976919813@gmail.com>
2025-01-10 07:28:43 +0800

    1140110

--- web/js/image.js
+++ web/js/image.js
@@ -21,7 +21,7 @@
     //第四個參數代表: onLeaveBack - 向上滾動，目標啟動點 與 視窗捲軸開始
 
     //gsap.registerPlugin(ScrollTrigger)
-    gsap.from("#pic2",{    
+    gsap.from("#contactpic",{    
         scrollTrigger:{
             trigger: "#three",
             start: "top 400px",
@@ -29,7 +29,7 @@
             toggleActions: "restart restart pause pause",//play, pause, resume, reverse, complete, restart, reset, or none.
             scrub: 1, //1 sec, or  srub:true start到end的時間
             //horizontal: true,
-            markers: false
+            markers: true
         },
         duration: 0.5,//no use if scrub:1
         opacity:0,
