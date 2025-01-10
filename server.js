df7c700aafb5ea74be5e6a22ea454e2fc2a3a332
Kagami5464 <a0976919813@gmail.com>
2025-01-10 22:56:35 +0800

    1140110

--- server.js
+++ server.js
@@ -9,21 +9,42 @@ server.listen(80, () => {
 
 
 var DB = require("nedb-promises");
-
-var profolioWorks = DB.create(__dirname+"/protfolioWorks.db");
+var profolioWorks = DB.create(__dirname+"/profolioWorks.db");
 
 profolioWorks.insert([
-    { imgUrl: "images/2.png", text: "Kuro" },
+    {imageUrl:'images/2.png',text:"Kuro"},
+    {imageUrl:'images/3.png',text:"Shusta"},
+    {imageUrl:'images/4.png',text:"Isagi"},
+    {imageUrl:'images/5.png',text:"Ayru"},
+    {imageUrl:'images/6.png',text:"Mysta"},
+    {imageUrl:'images/7.jpeg',text:"とーる"},
+    {imageUrl:'images/8.PNG',text:"Mysta"},
 ])
 
-
-server.get("/profolioWorks-data", (req,res)=>{
+server.get("/porfolioWorks-data", (req,res)=>{
     //DB
-    profolioWorks.find({}).then(results=>{
-        if(results !=null){
-            res.send(results);        
-        }else{
-            res.send("Error!");
-        }
+    Works.find({}).then(results=>{
+      if(results != null){
+           res.send(results);
+      }else{
+          res.send("Error!");
+      }
     })
 })
+
+server.get("/porfolioWorks-data", (req,res)=>{
+    //DB
+    profolioWorks.find({}).then(results=>{
+      if(results != null){
+           res.send(results);
+      }else{
+          res.send("Error!");
+      }
+    })
+})
+
+
+
+server.listen(80, () => {
+    console.log("Server is running at port 80.");
+});
